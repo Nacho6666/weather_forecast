6b61baafea229bf904c47b409de0b4a3e045a56d
Nacho6666 <111170094+Nacho6666@users.noreply.github.com>
2023-11-27 14:44:21 +0800

    cancel token and no debug mode

--- lib/http/place_service.dart
+++ lib/http/place_service.dart
@@ -14,7 +14,7 @@ class PlaceSuggestionsNotifier extends StateNotifier<List<String>> {
   PlaceSuggestionsNotifier(this.ref) : super([]);
 
   Future<void> fetchSuggestions(String input) async {
-    String trimmedInput = input.trim();
+    String trimmedInput = input.trim(); //輸入空格不處理 減少 api 呼叫
     if (trimmedInput.isNotEmpty && trimmedInput != state){
       final placeApi = ref.read(placeApiProvider);
       try {
@@ -31,6 +31,7 @@ class PlaceApiProvider {
   final Dio client;
 
   PlaceApiProvider(this.sessionToken) : client = Dio();
+  CancelToken _cancelToken = CancelToken();
 
   final sessionToken;
 
@@ -38,6 +39,11 @@ class PlaceApiProvider {
 
   Future<List<String>> fetchSuggestions(String input) async {
     if(input.isNotEmpty){
+      if (!_cancelToken.isCancelled) {
+        _cancelToken.cancel();
+      }
+      _cancelToken = CancelToken();
+
       try {
         final response = await client.get(
           'https://maps.googleapis.com/maps/api/place/autocomplete/json',
@@ -46,20 +52,21 @@ class PlaceApiProvider {
             'language': 'zh-TW',
             'key': apiKey,
           },
+          cancelToken: _cancelToken,
         );
-        print("PlaceApi Provider Response: ${response.data}");
-        if (response.statusCode == 200) {
-          final List predictions = response.data['predictions'];
+              print("PlaceApi Provider Response: ${response.data}");
+              if (response.statusCode == 200) {
+                final List predictions = response.data['predictions'];
 
-          return [input] + await predictions.where((prediction) {
-            bool isCityOrCountry = prediction['types'].any((type) => type == 'country' || type == 'locality');
-            return isCityOrCountry;
-          }).map<String>((prediction) => prediction['description']).toList();
-        } else {
-          return ['Failed to fetch suggestions'];
-        }
+                return [input] + await predictions.where((prediction) {
+                  bool isCityOrCountry = prediction['types'].any((type) => type == 'country' || type == 'locality');
+                  return isCityOrCountry;
+                }).map<String>((prediction) => prediction['description']).toList();
+              } else {
+                return ['Failed to fetch suggestions'];
+              }
       } catch (e) {
-        print('Error occurred: $e');
+          print('Error occurred: $e');
         rethrow;
       }
     } else {
